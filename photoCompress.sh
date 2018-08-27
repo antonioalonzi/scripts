f0f0ff47bd3ae45b7a1e4626a0b9c9a52472c90c
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-08-27 20:44:37 +0100

    Improving photoCompress Script.

--- photoCompress.sh
+++ photoCompress.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-for file in *.[jJ][pP][gG] *.[jJ][pP][eE][gG]
-do
-	mkdir -p resized
+
+# Create resized folder
+mkdir -p resized
+
+# Count photos
+count=$(ls -l *.[jJ][pP][gG] 2>/dev/null | wc -l)
+echo -e "Found $count Photos.\n"
+
+# Resize photos
+i=1
+for file in *.[jJ][pP][gG]
+do		
+	echo "Converting [$i out $count]: $file"
 	convert "$file" -resize 4320000@ "resized/$file"
+	let i=i+1
 done
+
+# Count unprocessed photos
+count=$(ls -l *.[jJ][pP][eE][gG] 2>/dev/null | wc -l)
+echo -e "\nIgnored $count JPEG Photos."
+
+
